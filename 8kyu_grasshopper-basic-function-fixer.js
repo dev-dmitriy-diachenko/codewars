ca0885b86e013fcaf659e086251888376219c215
dev-dmitriy-diachenko <dev.dmitriy.diachenko@gmail.com>
2022-09-08 15:41:38 +0300

    add kata solution

---
+++ 8kyu_grasshopper-basic-function-fixer.js
@@ -0,0 +1,15 @@
+/* Task:
+
+Fix the function
+I created this function to add five to any number that was passed in to it and return the new value. It doesn't throw any errors but it returns the wrong number.
+
+Can you help me fix the function?
+
+*/
+
+const addFive = (num) => num + 5;
+	
+// Simple tests:
+console.log(addFive(5), ' => 10');
+console.log(addFive(0), ' => 5');
+console.log(addFive(-5), ' => 0');
