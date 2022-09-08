927509fffc4d3a2300f211d2b402563af1dd54c7
dev-dmitriy-diachenko <dev.dmitriy.diachenko@gmail.com>
2022-09-08 11:32:32 +0300

    add kata solution

---
+++ 8kyu_basic-training-add-item-to-an-array.js
@@ -0,0 +1,16 @@
+/* Task:
+
+Add the value "codewars" to the websites array.
+After your code executes the websites array should == ["codewars"]
+
+The websites array has already been defined for you using the following code:
+var websites = [];
+
+*/
+
+const websites = [];
+
+websites.push('codewars');
+
+// Simple tests:
+console.log(websites, websites.includes('codewars'))
