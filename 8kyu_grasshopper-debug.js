cfd81213cbdf7966dbcbb70398a351645bc2c502
dev-dmitriy-diachenko <dev.dmitriy.diachenko@gmail.com>
2022-09-08 09:34:22 +0300

    add kata solution

---
+++ 8kyu_grasshopper-debug.js
@@ -0,0 +1,27 @@
+/* Task:
+
+Debug celsius converter
+Your friend is traveling abroad to the United States so he wrote a program to convert fahrenheit to celsius. Unfortunately his code has some bugs.
+
+Find the errors in the code to get the celsius converter working properly.
+
+To convert fahrenheit to celsius:
+
+celsius = (fahrenheit - 32) * (5/9)
+Remember that typically temperatures in the current weather conditions are given in whole numbers. It is possible for temperature sensors to report temperatures with a higher accuracy such as to the nearest tenth. Instrument error though makes this sort of accuracy unreliable for many types of temperature measuring sensors.
+
+*/
+
+const weatherInfo = (temp) => {
+	const celsius = convertToCelsius(temp);
+
+	return celsius < 0
+		? `${celsius} is freezing temperature`
+		: `${celsius} is above freezing temperature`;
+};
+
+const convertToCelsius = (temperature) => (temperature - 32) * (5 / 9);
+
+// Simple tests:
+console.log(weatherInfo(50), ' => 10 is above freezing temperature')
+console.log(weatherInfo(23),  ' => -5 is freezing temperature')
