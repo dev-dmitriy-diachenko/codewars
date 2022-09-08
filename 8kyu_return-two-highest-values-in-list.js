73d720c0fe68c1ce9bdf3b637b2d4be9564166d3
dev-dmitriy-diachenko <dev.dmitriy.diachenko@gmail.com>
2022-09-08 16:08:23 +0300

    add kata solution

--- 8kyu_return-two-highest-values-in-list.js
+++ 8kyu_return-two-highest-values-in-list.js
@@ -15,6 +15,6 @@ Examples:
 const twoHighest = (arr) => [...new Set(arr.sort((a, b) => b - a))].slice(0, 2);
 
 // Simple tests:
-console.log(twoHighest([]), '=> []')
-console.log(twoHighest([15]), '=> [15]')
-console.log(twoHighest([15, 20, 20, 17]), '=> [20, 17]')
+console.log(twoHighest([]), '=> []');
+console.log(twoHighest([15]), '=> [15]');
+console.log(twoHighest([15, 20, 20, 17]), '=> [20, 17]');
