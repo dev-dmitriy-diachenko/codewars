5b4bb0d50d608275721c607cb07e7f89b01c0482
dev-dmitriy-diachenko <dev.dmitriy.diachenko@gmail.com>
2022-09-08 09:12:24 +0300

    add kata solution

---
+++ 8kyu_get-planet-name-by-id.js
@@ -0,0 +1,31 @@
+/* Task:
+
+The function is not returning the correct values. Can you figure out why?
+
+*/
+
+const getPlanetName = (id) => {
+	switch (id) {
+		case 1:
+			return 'Mercury'
+		case 2:
+			return 'Venus'
+		case 3:
+			return 'Earth'
+		case 4:
+			return 'Mars'
+		case 5:
+			return 'Jupiter'
+		case 6:
+			return 'Saturn'
+		case 7:
+			return 'Uranus'
+		case 8:
+			return 'Neptune'
+	};
+};
+
+// Simple tests:
+console.log(getPlanetName(2), ' => Venus');
+console.log(getPlanetName(5), ' => Jupiter');
+console.log(getPlanetName(3), ' => Earth');
