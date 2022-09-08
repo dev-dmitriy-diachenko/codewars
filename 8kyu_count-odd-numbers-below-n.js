7b3ba4d7d0c456156180c30346baf6d36ed5f992
dev-dmitriy-diachenko <dev.dmitriy.diachenko@gmail.com>
2022-09-08 10:54:54 +0300

    add kata solution

---
+++ 8kyu_count-odd-numbers-below-n.js
@@ -0,0 +1,11 @@
+/* Task:
+
+Given a number n, return the number of positive odd numbers below n, EASY!
+
+*/
+
+const oddCount = (n) => Math.floor(n / 2);
+
+// Simple tests:
+console.log(oddCount(15), ' => 7')
+console.log(oddCount(15023),  ' => 7511')
