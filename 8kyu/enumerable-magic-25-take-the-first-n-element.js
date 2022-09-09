d67e578e661e8e0522edcaae7aa44baca95f6386
dev-dmitriy-diachenko <dev.dmitriy.diachenko@gmail.com>
2022-09-09 07:57:10 +0300

    rename all current files

---
+++ 8kyu/enumerable-magic-25-take-the-first-n-element.js
@@ -0,0 +1,10 @@
+/* Task:
+
+Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
+
+*/
+
+const take = (arr, n) => arr.slice(0, n);
+	
+// Simple tests:
+console.log(take([0,1,2,3,4,5,6], 5), ' => [0, 1, 2, 3, 4]')
