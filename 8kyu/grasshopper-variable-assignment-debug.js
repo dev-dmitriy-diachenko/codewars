e6d617123b0c72b9b74d1b533103df47271c1158
dev-dmitriy-diachenko <dev.dmitriy.diachenko@gmail.com>
2022-09-09 08:55:30 +0300

    add kata solution

---
+++ 8kyu/grasshopper-variable-assignment-debug.js
@@ -0,0 +1,14 @@
+/* Task:
+
+Variable assignment
+Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+
+*/
+
+const a = "dev";
+const b = "Lab";
+
+let n = a + b;
+	
+// Simple tests:
+console.log(n === 'devLab');
