549b97fa65d4fc6378c4a8360a9f29538abb1f9c
dev-dmitriy-diachenko <dev.dmitriy.diachenko@gmail.com>
2022-09-09 12:51:37 +0300

    add kata solution

---
+++ 8kyu/are-arrow-functions-odd.js
@@ -0,0 +1,16 @@
+/* Task:
+
+Time to test your basic knowledge in functions! Return the odds from a list:
+
+[1, 2, 3, 4, 5]  -->  [1, 3, 5]
+[2, 4, 6]        -->  []
+
+*/
+
+const odds = (values) => values.filter(value => value % 2);
+	
+// Simple tests:
+console.log(odds([]), ' => []');
+console.log(odds([2, 4, 6]), ' => []');
+console.log(odds([1, 3, 5]), ' => [1, 3, 5]');
+console.log(odds([1, 2, 3, 4, 5, 6]), ' => [1, 3, 5]');
