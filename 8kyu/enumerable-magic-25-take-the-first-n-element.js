77b326c8afd96d3ec5a33d48fd4c23857b99d078
dev-dmitriy-diachenko <dev.dmitriy.diachenko@gmail.com>
2022-09-09 08:11:02 +0300

    add kata solution

--- 8kyu/enumerable-magic-25-take-the-first-n-element.js
+++ 8kyu/enumerable-magic-25-take-the-first-n-element.js
@@ -7,4 +7,4 @@ Create a function that accepts a list/array and a number n, and returns a list/a
 const take = (arr, n) => arr.slice(0, n);
 	
 // Simple tests:
-console.log(take([0,1,2,3,4,5,6], 5), ' => [0, 1, 2, 3, 4]')
+console.log(take([0, 1, 2, 3, 4, 5, 6], 5), ' => [0, 1, 2, 3, 4]');
