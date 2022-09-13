4e4a102367870d85209a37b0124ba7e509dce34c
dev-dmitriy-diachenko <dev.dmitriy.diachenko@gmail.com>
2022-09-13 20:26:44 +0300

    add kata solution

---
+++ 8kyu/return-the-day.js
@@ -0,0 +1,26 @@
+/* Task:
+
+Complete the function which returns the weekday according to the input number:
+
+1 returns "Sunday"
+2 returns "Monday"
+3 returns "Tuesday"
+4 returns "Wednesday"
+5 returns "Thursday"
+6 returns "Friday"
+7 returns "Saturday"
+
+Otherwise returns "Wrong, please enter a number between 1 and 7"
+
+*/
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+const whatday = (num) => days[num - 1] || 'Wrong, please enter a number between 1 and 7';
+
+// Simple tests:
+console.log(whatday(1) === 'Sunday');
+console.log(whatday(2) === 'Monday');
+console.log(whatday(3) === 'Tuesday');
+console.log(whatday(8) === 'Wrong, please enter a number between 1 and 7');
+console.log(whatday(20) === 'Wrong, please enter a number between 1 and 7');
