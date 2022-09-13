2bb02880e3d16829e7dbe805c91e09d0ae4ed968
dev-dmitriy-diachenko <dev.dmitriy.diachenko@gmail.com>
2022-09-13 21:50:29 +0300

    add kata solution

---
+++ 8kyu/training-js-18-mathods-of-string-object-concat-split-join.js
@@ -0,0 +1,23 @@
+/* Task:
+
+Implement a function which accepts 2 arguments: string and separator.
+
+The expected algorithm: split the string into words by spaces, split each word into separate characters and join them back with the specified separator, join all the resulting "words" back into a sentence with spaces.
+
+For example:
+
+splitAndMerge("My name is John", " ")  ==  "M y n a m e i s J o h n"
+splitAndMerge("My name is John", "-")  ==  "M-y n-a-m-e i-s J-o-h-n"
+splitAndMerge("Hello World!", ".")     ==  "H.e.l.l.o W.o.r.l.d.!"
+splitAndMerge("Hello World!", ",")     ==  "H,e,l,l,o W,o,r,l,d,!"
+
+*/
+
+const splitAndMerge = (string, separator) =>
+	string.split(' ').map(el => [...el].join(separator)).join(' ');
+
+// Simple tests:
+console.log(splitAndMerge("My name is John"," ") === "M y n a m e i s J o h n");
+console.log(splitAndMerge("My name is John","-") === "M-y n-a-m-e i-s J-o-h-n");
+console.log(splitAndMerge("Hello World!",".") === "H.e.l.l.o W.o.r.l.d.!");
+console.log(splitAndMerge("Hello World!",",") === "H,e,l,l,o W,o,r,l,d,!");
