b2700f4e1a37ba06a53f504ab50357d081bacad3
dev-dmitriy-diachenko <dev.dmitriy.diachenko@gmail.com>
2022-09-14 22:20:29 +0300

    add kata solution

---
+++ 8kyu/power.js
@@ -0,0 +1,28 @@
+/* Task:
+
+The goal is to create a function 'numberToPower(number, power)' that "raises" the number up to power (ie multiplies number by itself power times).
+
+Examples
+numberToPower(3, 2)  // -> 9 ( = 3 * 3 )
+numberToPower(2, 3)  // -> 8 ( = 2 * 2 * 2 )
+numberToPower(10, 6) // -> 1000000
+
+Note: Math.pow and some other Math functions like eval() and ** are disabled.
+
+*/
+
+const numberToPower = (number, power) => {
+	let result = 1;
+
+	while (power) {
+		result *= number;
+		power--;
+	};
+
+	return result;
+};
+	
+// Simple tests:
+console.log(numberToPower(4, 2) === 16);
+console.log(numberToPower(10, 4) === 10000);
+console.log(numberToPower(10, 0) === 1);
