eb0ed172e9e3d8c61e721c89eb2f02499f396c2b
dev-dmitriy-diachenko <dev.dmitriy.diachenko@gmail.com>
2022-09-14 09:50:38 +0300

    add kata solution

---
+++ 8kyu/equals.js
@@ -0,0 +1,16 @@
+/* Task:
+
+You task to pass only this tests :
+
+a == false
+!a == false
+a == !a
+
+*/
+
+const a = [];
+
+// Simple tests:
+console.log(a == false);
+console.log(!a == false);
+console.log(a == !a); 
