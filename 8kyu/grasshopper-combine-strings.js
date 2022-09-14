0839c0165c2cb48a39f3e26f0e517aa95fd788c0
dev-dmitriy-diachenko <dev.dmitriy.diachenko@gmail.com>
2022-09-14 12:59:56 +0300

    add kata solution

---
+++ 8kyu/grasshopper-combine-strings.js
@@ -0,0 +1,18 @@
+/* Task:
+
+Combine strings function
+Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
+
+Example:
+
+combineNames('James', 'Stevens')
+returns:
+
+'James Stevens'
+
+*/
+
+const combineNames = (firstName, lastName) => `${firstName} ${lastName}`;
+
+// Simple tests:
+console.log(combineNames('James', 'Stevens') === 'James Stevens');
