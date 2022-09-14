cdadd4f05e5d9446107cabbcf151bd01eb37c079
dev-dmitriy-diachenko <dev.dmitriy.diachenko@gmail.com>
2022-09-14 11:21:08 +0300

    add kata solution

--- 8kyu/multiply-the-number.js
+++ 8kyu/multiply-the-number.js
@@ -1,6 +1,6 @@
 /* Task:
 
-ack really likes his number five: the trick here is that you have to multiply each number by 5 raised to the number of digits of each numbers, so, for example:
+Jack really likes his number five: the trick here is that you have to multiply each number by 5 raised to the number of digits of each numbers, so, for example:
 
 multiply(3)==15 // 3 * 5¹
 multiply(10)==250 // 10 * 5²
