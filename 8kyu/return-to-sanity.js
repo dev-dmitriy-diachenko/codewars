bde58559bfff98228f562b2c1f12e32c9719f32b
dev-dmitriy-diachenko <dev.dmitriy.diachenko@gmail.com>
2022-09-14 23:32:49 +0300

    add kata solution

---
+++ 8kyu/return-to-sanity.js
@@ -0,0 +1,12 @@
+/* Task:
+
+This function should return an object, but it's not doing what's intended. What's wrong?
+
+*/
+
+const mystery = () => {
+	return { sanity: 'Hello' };
+};
+
+// Simple tests:
+console.log(mystery(), " => {sanity: 'Hello'}");
