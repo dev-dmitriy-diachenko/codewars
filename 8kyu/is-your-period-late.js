356f807a21e024480e2c86d10db026859a2c8ec4
dev-dmitriy-diachenko <dev.dmitriy.diachenko@gmail.com>
2022-09-14 14:01:49 +0300

    add kata solution

---
+++ 8kyu/is-your-period-late.js
@@ -0,0 +1,22 @@
+/* Task:
+
+In this kata, we will make a function to test whether a period is late.
+
+Our function will take three parameters:
+
+last - The Date object with the date of the last period
+
+today - The Date object with the date of the check
+
+cycleLength - Integer representing the length of the cycle in days
+
+Return true if the number of days passed from last to today is greater than cycleLength. Otherwise, return false.
+
+*/
+
+const periodIsLate = (last, today, cycleLength) => 
+	Date.parse(last) + cycleLength * 8.64e+7 < Date.parse(today);
+
+// Simple tests:
+console.log(periodIsLate(new Date(2016, 6, 13), new Date(2016, 7, 16), 35) === false);
+console.log(periodIsLate(new Date(2016, 6, 13), new Date(2016, 7, 16), 28) === true);
--- 8kyu/leonardo-dicaprio-and-oscars.js
+++ 8kyu/leonardo-dicaprio-and-oscars.js
@@ -26,7 +26,7 @@ const leo = (oscar) => {
 };
 
 // Simple tests:
-console.log(leo(89) === "Leo got one already!")
+console.log(leo(89) === "Leo got one already!");
 console.log(leo(88) === "Leo finally won the oscar! Leo is happy");
-console.log(leo(87) === "When will you give Leo an Oscar?")
-console.log(leo(86) === "Not even for Wolf of wallstreet?!")
+console.log(leo(87) === "When will you give Leo an Oscar?");
+console.log(leo(86) === "Not even for Wolf of wallstreet?!");
