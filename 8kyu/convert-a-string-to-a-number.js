d00046eb4f15aa6a81ef770504ed2de43b2d0312
dev-dmitriy-diachenko <dev.dmitriy.diachenko@gmail.com>
2022-09-15 14:35:48 +0300

    add kata solution

---
+++ 8kyu/convert-a-string-to-a-number.js
@@ -0,0 +1,19 @@
+/* Task:
+
+Note: This kata is inspired by Convert a Number to a String!. Try that one too.
+
+Description
+
+We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+
+*/
+
+const stringToNumber = (str) => +str;
+
+// Simple tests:
+console.log(stringToNumber("1234") ===1234);
+console.log(stringToNumber("605") === 605);
+console.log(stringToNumber("1405") ===1405);
+console.log(stringToNumber("-7") ===  -7);
