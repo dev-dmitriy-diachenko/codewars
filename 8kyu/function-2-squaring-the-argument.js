ee9959dc23c2b12e9e38f783cbe39a403c2501e1
dev-dmitriy-diachenko <dev.dmitriy.diachenko@gmail.com>
2022-09-15 11:34:19 +0300

    add kata solution

---
+++ 8kyu/function-2-squaring-the-argument.js
@@ -0,0 +1,10 @@
+/* Task:
+
+Now you have to write a function that takes an argument and returns the square of it.
+
+*/
+
+const square = num => num ** 2;
+
+// Simple tests:
+console.log(square(3) === 9);
