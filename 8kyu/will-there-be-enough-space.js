126eaf1035eb6c722800b2ab76be934f30de1509
dev-dmitriy-diachenko <dev.dmitriy.diachenko@gmail.com>
2022-09-15 12:24:15 +0300

    add kata solution

---
+++ 8kyu/will-there-be-enough-space.js
@@ -0,0 +1,23 @@
+/* Task:
+
+Bob is working as a bus driver. However, he has become extremely popular amongst the city's residents. With so many passengers wanting to get aboard his bus, he sometimes has to face the problem of not enough space left on the bus! He wants you to write a simple program telling him if he will be able to fit all the passengers.
+
+Task Overview:
+
+You have to write a function that accepts three parameters:
+
+cap is the amount of people the bus can hold excluding the driver.
+on is the number of people on the bus excluding the driver.
+wait is the number of people waiting to get on to the bus excluding the driver.
+
+If there is enough space, return 0, and if there isn't, return the number of passengers he can't take.
+
+*/
+
+const enough = (cap, on, wait) =>
+	on + wait < cap ? 0 : on + wait - cap;
+
+// Simple tests:
+console.log(enough(10, 5, 5) === 0);
+console.log(enough(100, 60, 50) === 10);
+console.log(enough(20, 5, 5) === 0);
