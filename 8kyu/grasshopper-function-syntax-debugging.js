b15af97d65eddb9193ac68e421fe5d4461a7b133
dev-dmitriy-diachenko <dev.dmitriy.diachenko@gmail.com>
2022-09-15 10:50:15 +0300

    add kata solution

---
+++ 8kyu/grasshopper-function-syntax-debugging.js
@@ -0,0 +1,11 @@
+/* Task:
+
+A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+*/
+
+const main = (verb, noun) => verb + noun;
+
+// Simple tests:   
+console.log(main('take ', 'item') === 'take item');
+console.log(main('use ', 'sword') === 'use sword');
