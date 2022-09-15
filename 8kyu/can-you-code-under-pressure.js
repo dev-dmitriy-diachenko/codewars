eea5d2257e510ed12914dcd51b1fd352eab61fea
dev-dmitriy-diachenko <dev.dmitriy.diachenko@gmail.com>
2022-09-15 13:51:28 +0300

    add kata solution

---
+++ 8kyu/can-you-code-under-pressure.js
@@ -0,0 +1,10 @@
+/* Task:
+
+Code as fast as you can! You need to double the integer and return it.
+
+*/
+
+const doubleInteger = (num) => num * 2;
+
+// Simple tests:
+console.log(doubleInteger(2) == 4);
