ec68921e448baede10a2f3c43bfb8e04fcfcc60e
dev-dmitriy-diachenko <dev.dmitriy.diachenko@gmail.com>
2022-09-15 14:34:02 +0300

    add kata solution

---
+++ 8kyu/oposite-number.js
@@ -0,0 +1,15 @@
+/* Task:
+
+Very simple, given an integer or a floating-point number, find its opposite.
+
+*/
+
+const opposite = (number) => -number;
+
+// Simple tests:
+console.log(opposite(1) === -1);
+console.log(opposite(0) === 0);
+console.log(opposite(4.25) === -4.25);
+console.log(opposite(3.3333333) === -3.3333333);
+console.log(opposite(-12525220.3325) === 12525220.3325);
+console.log(opposite(-5) === 5);
