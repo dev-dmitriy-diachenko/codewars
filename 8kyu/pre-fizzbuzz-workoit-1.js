62ddf326ec5d3d5c0d3b91c0c29768b044a495ca
dev-dmitriy-diachenko <dev.dmitriy.diachenko@gmail.com>
2022-09-15 11:01:46 +0300

    add kata solution

---
+++ 8kyu/pre-fizzbuzz-workoit-1.js
@@ -0,0 +1,28 @@
+/* Task:
+
+This is the first step to understanding FizzBuzz.
+
+Your inputs: a positive integer, n, greater than or equal to one. n is provided, you have NO CONTROL over its value.
+
+Your expected output is an array of positive integers from 1 to n (inclusive).
+
+Your job is to write an algorithm that gets you from the input to the output.
+
+*/
+
+const preFizz = (num) => {
+	const res = [];
+
+	for (let i = 1; i <= num; i++) {
+		res.push(i)
+	};
+
+	return res;
+};
+
+// Simple tests:   
+console.log(preFizz(1), " => [1]");
+console.log(preFizz(2), " => [1,2]");
+console.log(preFizz(3), " => [1,2,3]");
+console.log(preFizz(4), " => [1,2,3,4]");
+console.log(preFizz(5), " => [1,2,3,4,5]");
