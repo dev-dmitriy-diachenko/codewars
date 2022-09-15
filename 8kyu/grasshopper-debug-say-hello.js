5ea4daa1bd483adbe1882c788ca583e5832b674e
dev-dmitriy-diachenko <dev.dmitriy.diachenko@gmail.com>
2022-09-15 11:29:13 +0300

    add kata solution

---
+++ 8kyu/grasshopper-debug-say-hello.js
@@ -0,0 +1,13 @@
+/* Task:
+
+The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+*/
+
+const sayHello = (name) => `Hello, ${name}`;
+
+// Simple tests:
+console.log(sayHello('Mr. Spock') === 'Hello, Mr. Spock');
+console.log(sayHello('Captain Kirk') === 'Hello, Captain Kirk');
+console.log(sayHello('Liutenant Uhura') === 'Hello, Liutenant Uhura');
+console.log(sayHello('Dr. McCoy') === 'Hello, Dr. McCoy') ;
