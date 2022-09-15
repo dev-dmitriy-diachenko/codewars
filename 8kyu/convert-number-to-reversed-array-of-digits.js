636345777ac0a55749284c7d474c0227f1fc49b8
dev-dmitriy-diachenko <dev.dmitriy.diachenko@gmail.com>
2022-09-15 13:09:15 +0300

    add kata solution

---
+++ 8kyu/convert-number-to-reversed-array-of-digits.js
@@ -0,0 +1,11 @@
+/* Task:
+
+Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+*/
+
+const digitize = n => [...n.toString()].reverse().map(Number);
+
+// Simple tests:
+console.log(digitize(35231), " => [1, 3, 2, 5, 3]");
+console.log(digitize(0), " => [0]");
