5c473fc73229ea3c0487a4d6e0a52fbdd6345444
dev-dmitriy-diachenko <dev.dmitriy.diachenko@gmail.com>
2022-09-15 14:52:55 +0300

    add kata solution

---
+++ 8kyu/multiply.js
@@ -0,0 +1,13 @@
+/* Task:
+
+This code does not execute properly. Try to figure out why.
+
+*/
+
+const multiply = (a, b) => a * b;
+
+// Simple tests:
+console.log(multiply(1, 1) === 1);
+console.log(multiply(2, 1) === 2);
+console.log(multiply(2, 2) === 4);
+console.log(multiply(3, 5) === 15); 
