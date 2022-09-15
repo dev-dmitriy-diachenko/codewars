ba934d36eb60c0710023797d14532ca213225b09
dev-dmitriy-diachenko <dev.dmitriy.diachenko@gmail.com>
2022-09-15 13:18:25 +0300

    add kata solution

---
+++ 8kyu/grasshopper-personalized-message.js
@@ -0,0 +1,18 @@
+/* Task:
+
+Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+Use conditionals to return the proper message:
+
+case	return
+name equals owner	'Hello boss'
+otherwise	'Hello guest'
+
+*/
+
+const greet = (name, owner) =>
+	name === owner ? `Hello boss` : `Hello guest`;
+
+// Simple tests:
+console.log(greet('Daniel', 'Daniel') === 'Hello boss');
+console.log(greet('Greg', 'Daniel') === 'Hello guest');
--- 8kyu/reversed-strings.js
+++ 8kyu/reversed-strings.js
@@ -4,7 +4,7 @@ Complete the solution so that it reverses the string passed into it.
 
 */
 
-const solution = (str) => [...str].reverse().join('')
+const solution = (str) => [...str].reverse().join('');
 
 // Simple tests:
 console.log(solution('world') === 'dlrow');
