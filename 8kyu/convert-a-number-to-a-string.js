b2118b7f455319deb9f3f9629a18e1ae36a95b5a
dev-dmitriy-diachenko <dev.dmitriy.diachenko@gmail.com>
2022-09-15 14:38:57 +0300

    add kata solution

---
+++ 8kyu/convert-a-number-to-a-string.js
@@ -0,0 +1,12 @@
+/* Task:
+
+We need a function that can transform a number (integer) into a string.
+
+What ways of achieving this do you know?
+
+*/
+
+const numberToString = (num) => '' + num;
+
+// Simple tests:
+console.log(numberToString(67) === '67');
