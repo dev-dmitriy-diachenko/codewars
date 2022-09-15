be84be41893c823c4b6ca47938d943e332f719be
dev-dmitriy-diachenko <dev.dmitriy.diachenko@gmail.com>
2022-09-15 12:54:17 +0300

    add kata solution

---
+++ 8kyu/make-uppercase.js
@@ -0,0 +1,10 @@
+/* Task:
+
+Write a function which converts the input string to uppercase.
+
+*/
+
+const makeUpperCase = (str) => str.toUpperCase();
+
+// Simple tests:
+console.log(makeUpperCase("hello") === "HELLO");
