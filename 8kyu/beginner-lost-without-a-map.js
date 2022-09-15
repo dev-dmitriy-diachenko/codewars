09225838e010f3af3f6703c0691270b98d95894b
dev-dmitriy-diachenko <dev.dmitriy.diachenko@gmail.com>
2022-09-15 11:54:18 +0300

    add kata solution

---
+++ 8kyu/beginner-lost-without-a-map.js
@@ -0,0 +1,10 @@
+/* Task:
+
+Given an array of integers, return a new array with each value doubled.
+
+*/
+
+const maps = (x) => x.map(el => el * 2);
+
+// Simple tests:
+console.log(maps([1, 2, 3]), " => [2, 4, 6]");
