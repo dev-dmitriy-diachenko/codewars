fa165de6100b04a7f87bb3cb1f406f381f51533e
dev-dmitriy-diachenko <dev.dmitriy.diachenko@gmail.com>
2022-09-21 12:41:51 +0300

    add kata solution

--- 7kyu/vowel-count.js
+++ 7kyu/vowel-count.js
@@ -1,18 +1,14 @@
 /* Task:
 
-Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+Return the number (count) of vowels in the given string.
 
-Examples:
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
 
-Input: 42145 Output: 54421
-
-Input: 145263 Output: 654321
-
-Input: 123456789 Output: 987654321
+The input string will only consist of lower case letters and/or spaces.
 
 */
 
-const getCount = str => str.match(/[aeiou]/ig)?.length || 0;
+const getCount = (str) => str.match(/[aeiou]/ig)?.length || 0;
 
 // Simple tests:
-console.log(getCount("abracadabra") === 5) ;
+console.log(getCount("abracadabra") === 5);
