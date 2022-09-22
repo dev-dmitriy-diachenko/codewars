af1122c86d10834fc57c79c9c9a8f9ce94a0b4e2
dev-dmitriy-diachenko <dev.dmitriy.diachenko@gmail.com>
2022-09-22 11:30:47 +0300

    add kata solution

--- 6kyu/delete-occurances-of-an-element-if-it-occurs-more-than-n-times.js
+++ 6kyu/delete-occurances-of-an-element-if-it-occurs-more-than-n-times.js
@@ -32,5 +32,5 @@ const deleteNth = (arr, n) => {
 };
 
 // Simple tests:
-console.log(deleteNth([20,37,20,21], 1), " => [20,37,21]")
-console.log(deleteNth([1,1,3,3,7,2,2,2,2], 3), " => [1, 1, 3, 3, 7, 2, 2, 2]")
+console.log(deleteNth([20, 37, 20, 21], 1), " => [20,37,21]");
+console.log(deleteNth([1, 1, 3, 3, 7, 2, 2, 2, 2], 3), " => [1, 1, 3, 3, 7, 2, 2, 2]");
