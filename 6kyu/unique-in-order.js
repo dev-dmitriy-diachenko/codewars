03f4940e90457b3153300ff7bf011cfe536688b1
dev-dmitriy-diachenko <dev.dmitriy.diachenko@gmail.com>
2022-09-22 14:52:09 +0300

    add kata solution

---
+++ 6kyu/unique-in-order.js
@@ -0,0 +1,11 @@
+/* Task:
+
+Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+
+*/
+
+const uniqueInOrder = (iterable) =>
+	[...iterable].filter((a, i) => a !== iterable[i - 1]);
+
+// Simple tests:
+console.log(uniqueInOrder('AAAABBBCCDAABBB'), " => ['A','B','C','D','A','B']");
