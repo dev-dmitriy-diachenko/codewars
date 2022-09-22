db866dcf1786a024dd5b8c29bd4b9f543dfe5efc
dev-dmitriy-diachenko <dev.dmitriy.diachenko@gmail.com>
2022-09-22 10:42:22 +0300

    add kata solution

---
+++ 6kyu/count-the-smile-faces.js
@@ -0,0 +1,28 @@
+/* Task:
+
+Given an array (arr) as an argument complete the function countSmileys that should return the total number of smiling faces.
+
+Rules for a smiling face:
+
+- Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
+- A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
+- Every smiling face must have a smiling mouth that should be marked with either ) or D
+
+No additional characters are allowed except for those mentioned.
+
+Valid smiley face examples: :) :D ;-D :~)
+Invalid smiley faces: ;( :> :} :]
+
+*/
+
+const countSmileys = (smiles) => {
+  const filter = /^[:;][-~]?[)D]$/;
+
+	return smiles.filter((smile) => filter.test(smile)).length;
+};
+
+// Simple tests:
+console.log(countSmileys([]) === 0);
+console.log(countSmileys([':D',':~)',';~D',':)']) === 4);
+console.log(countSmileys([':)',':(',':D',':O',':;']) === 2);
+console.log(countSmileys([';]', ':[', ';*', ':$', ';-D']) === 1);
