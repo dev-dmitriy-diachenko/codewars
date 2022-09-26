fc47e0e409552eb9ed1e948b69d49c8ba5769bc3
dev-dmitriy-diachenko <dev.dmitriy.diachenko@gmail.com>
2022-09-26 11:49:40 +0300

    add kata solution

--- 5kyu/rgb-to-hex-conversion.js
+++ 5kyu/rgb-to-hex-conversion.js
@@ -1,6 +1,8 @@
 /* Task:
 
-Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+The rgb function is incomplete. Complete it so that passing in RGB decimal values will result in a hexadecimal representation being returned. Valid decimal values for RGB are 0 - 255. Any values that fall out of that range must be rounded to the closest valid value.
+
+Note: Your answer should always be 6 characters long, the shorthand with 3 will not work here.
 
 */
 
